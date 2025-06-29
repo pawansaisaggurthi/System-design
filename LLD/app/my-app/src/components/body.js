f7a95266e93845782f574deb6f3510c4db2464d8
jithendrasaggurthi <pawansaisaggurthi@gmail.com>
2025-06-29 12:20:44 +0530

    Infinite scroll

--- LLD/app/my-app/src/components/body.js
+++ LLD/app/my-app/src/components/body.js
@@ -5,24 +5,45 @@ import Shimmer from './Shimmer';
 
 const Body = () => {
 
-    const [memes, setMemes] = useState([])
+    const [memes, setMemes] = useState([]);
 
+    const [showShimmer, setShowShimmer] = useState(false)
 
     useEffect(() => {
-        fetchMemes()
+        fetchMemes();
+
+        window.addEventListener("scroll", handleScrollEvent);
+
+        return ()  => window.removeEventListener("scroll", handleScrollEvent)
     }, [])
 
+    const handleScrollEvent = () => {
+
+        // { we have did -1 in document.body.scrollheight body height 6393 and scrollevent height 6392.0666}
+
+        // "The 0.666 difference is due to sub-pixel rendering and floating-point math — scroll position can be fractional, but scrollHeight is usually rounded."
+
+
+        if (window.scrollY + window.innerHeight >= document.body.scrollHeight-1) {
+            fetchMemes();
+        }
+    }
+
     const fetchMemes = async () => {
+        setShowShimmer(true)
         const data = await fetch('https://meme-api.com/gimme/20')
-        const memes = await data.json();
-        setMemes(memes.memes)
+        const meme = await data.json();
+        setShowShimmer(false)
+        setMemes((memes) => [...memes, ...meme.memes])
     }
 
     return (
         <div className='flex flex-wrap'>
-            {memes.length === 0 ? <Shimmer /> : memes.map((meme, i) => (
+            {memes.map((meme, i) => (
                 <MemeCard key={i} data={meme} />
             ))}
+
+            {showShimmer && <Shimmer />}
         </div>
     )
 
